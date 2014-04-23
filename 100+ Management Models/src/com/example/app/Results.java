3e4e7d6a4af908c198c5ba85f00c349d8ad8b725
lbvdmeer <hahadielexieboy@msn.com>
2014-04-23 13:39:15 +0200

    Mag het nu?
    Ik wil syncen

--- 100+ Management Models/src/com/example/app/Results.java
+++ 100+ Management Models/src/com/example/app/Results.java
@@ -1,5 +1,78 @@
+<<<<<<< HEAD
 package com.example.app;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.MenuItem;
+import android.view.View;
+
+public class Results extends Activity {
+
+		@Override
+		protected void onCreate(Bundle savedInstanceState) {
+			super.onCreate(savedInstanceState);
+			setContentView(R.layout.activity_results);
+		}
+
+		@Override
+        public boolean onKeyDown(int keyCode, KeyEvent event)
+		{
+            if ((keyCode == KeyEvent.KEYCODE_BACK)) 
+            {
+                onBackPressed();
+                System.out.println("Back pressed");
+            }
+           
+			return true;
+		}
+		@Override
+	    public void onBackPressed()
+		{
+			Intent i = new Intent(Results.this, HomeScreen.class);
+			startActivity(i);
+			finish();
+	       
+	    }
+		
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+		    switch (item.getItemId()) {
+		        case android.R.id.home:
+		        	
+		        	//System.out.println("UP Pressed");
+		        	
+		        	Intent i = new Intent(Results.this, HomeScreen.class);
+		        	startActivity(i);
+		        	
+		        	finish();
+		        	
+		            return(true);
+		    }
+
+		    return(super.onOptionsItemSelected(item));
+		}
+		
+		public void onClickmod13(View view)
+		{
+			Intent i = new Intent(Results.this, ResultsMod13.class);
+			startActivity(i);
+			
+			finish();
+		}
+		
+		public void onClickmod14(View view)
+		{
+			Intent i = new Intent(Results.this, ResultsMod14.class);
+			startActivity(i);
+			
+			finish();
+		}
+=======
+package com.example.app;
+
+import android.R;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -51,4 +124,5 @@ public class Results extends Activity {
 
 		    return(super.onOptionsItemSelected(item));
 		}
+>>>>>>> 5283a02f783aacb1250c5d4c12c783b66d09504d
 }
