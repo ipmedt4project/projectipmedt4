07dff9d875d7ac3c2fc3ca38058950b2b67f8248
Zhoukov <tinusie_93@hotmail.com>
2014-04-23 12:40:10 +0200

    Merge branch 'master' of https://github.com/ipmedt4project/projectipmedt4
    Conflicts:
    	100+ Management Models/bin/AndroidManifest.xml
    	100+ Management Models/bin/classes/com/example/app/CustomerMod10.class
    	100+ Management Models/bin/classes/com/example/app/CustomerMod9.class
    	100+ Management Models/bin/classes/com/example/app/DatabaseModels.class
    	100+ Management Models/bin/classes/com/example/app/HomeScreen.class
    	100+ Management Models/bin/classes/com/example/app/R$id.class
    	100+ Management Models/bin/classes/com/example/app/R$layout.class
    	100+ Management Models/bin/classes/com/example/app/R$menu.class
    	100+ Management Models/bin/classes/com/example/app/R$string.class
    	100+ Management Models/bin/classes/com/example/app/R$style.class
    	100+ Management Models/gen/com/example/app/R.java
    	100+ Management Models/src/com/example/app/CustomerMod10.java
    	100+ Management Models/src/com/example/app/CustomerMod9.java

--- 100+ Management Models/src/com/example/app/Customer.java
+++ 100+ Management Models/src/com/example/app/Customer.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.MenuItem;
+import android.view.View;
 
 public class Customer extends Activity {
 
@@ -50,6 +51,22 @@ public class Customer extends Activity {
 		    }
 
 		    return(super.onOptionsItemSelected(item));
+		}
+		
+		public void onClickmod9(View view)
+		{
+			Intent i = new Intent(Customer.this, CustomerMod9.class);
+			startActivity(i);
+			
+			finish();
+		}
+		
+		public void onClickmod10(View view)
+		{
+			Intent i = new Intent(Customer.this, CustomerMod10.class);
+			startActivity(i);
+			
+			finish();
 		}
 		
 }
